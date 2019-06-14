1cbae421e2e562626ec810192bfef79904cb9e87
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-06-14 09:42:42 -0700

    checking in completed prototypes assignment

--- challenges/functions.js
+++ challenges/functions.js
@@ -24,16 +24,17 @@ const desk = ['Pen', 'Journal', 'Laptop', 'Phone', 'Paper', 'Water Bottle'];
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(x, y, consume) {
-  consume(x+y);
-}
+// function add(x, y, consume) {
+//   consume(x,y);
+// }
+
 
 
 
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
+// consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
